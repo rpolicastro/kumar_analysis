ef100ed204540ccdc09dcc46d4551581628d922a
rpolicastro <rpolicas@indiana.edu>
2019-07-03 11:24:09 -0400

    preparing counts file for seurat

--- Ariss_2018/seurat_analysis.R
+++ Ariss_2018/seurat_analysis.R
@@ -7,4 +7,12 @@ library("Seurat")
 ## scRNA-seq Analysis of Ariss et al., 2018
 ############################################
 
+## Preparing counts files.
 
+counts <- list.files("counts", pattern="*DMS") %>%
+	file.path("counts",.) %>%
+	map(
+		~ read.delim(., sep="\t", header=T, stringsAsFactors=F) %>%
+		column_to_rownames("GENE") %>%
+		as.matrix
+	)
