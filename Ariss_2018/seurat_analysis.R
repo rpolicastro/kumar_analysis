7bf10bbb06e896552b4586e695f140f116db9b62
rpolicastro <rpolicas@indiana.edu>
2019-07-03 14:46:06 -0400

    QC of cells

--- Ariss_2018/seurat_analysis.R
+++ Ariss_2018/seurat_analysis.R
@@ -8,11 +8,41 @@ library("Seurat")
 ############################################
 
 ## Preparing counts files.
+## ----------
 
-counts <- list.files("counts", pattern="*DMS") %>%
-	file.path("counts",.) %>%
+count.files <- list.files("counts", pattern="*DMS") %>% file.path("counts",.) %>%
+	setNames(basename(.) %>% substr(., 1, nchar(.)-9)) %>%
+	split(., names(.)) %>% lapply(., unname) %>%
 	map(
 		~ read.delim(., sep="\t", header=T, stringsAsFactors=F) %>%
 		column_to_rownames("GENE") %>%
-		as.matrix
+		as.sparse
 	)
+
+## Creating seurat objects.
+## ----------
+
+## Create object.
+
+seurat.obj <- map(counts, ~CreateSeuratObject(counts=.,))
+
+## Basic cell quality control.
+
+# Plotting number of genes and read counts.
+pdf("QC_violin-plot.pdf")
+map(
+	names(count.files),
+	~ VlnPlot(seurat.obj[[.]], features=c("nCount_RNA", "nFeature_RNA"), ncol=2)
+)
+dev.off()
+
+# Filtering out cells by outlying number of reads and read counts.
+seurat.obj <- map(
+	seurat.obj,
+	~ subset(., subset =
+		nFeature_RNA > 250 &
+		nFeature_RNA < 1000 &
+		nCount_RNA > 500 &
+		nCount_RNA < 5000
+	)
+)
