0154413f1821012e6f58f973788dfc4c95ee67bc
rpolicastro <rpolicas@indiana.edu>
2019-07-03 08:52:15 -0400

    starting main script

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###################
+## Kumar scRNA-seq
+###################
+
+## Downloading and Preparing Genome Annotation and Assembly
+## ----------
+
+if [ ! -d "./genome" ] && mkdir -p "./genome"
